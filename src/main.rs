5203418fbef10f514ece8ede6db3529aabe3432c
H2Owater425 <h2o@dhmo.kr>
2024-09-03 09:08:04 +0900

    feat: Use get_response

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod constant;
 mod time;
 mod logger;
+mod http;
 
 use std::{error::Error, io::{BufRead, BufReader, Lines, Write}, net::{Ipv4Addr, SocketAddrV4, TcpListener, TcpStream}};
+use http::{HttpStatus, get_response};
 use logger::Logger;
-use time::Time;
 
 fn main() -> Result<(), Box<dyn Error>> {
 	let listener: TcpListener = TcpListener::bind(SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 80))?;
@@ -17,7 +18,23 @@ fn main() -> Result<(), Box<dyn Error>> {
 		if let Some(Ok(request_uri)) = lines.next() {
 			logger.info(&request_uri)?;
 			
-			stream.write_all(format!("HTTP/1.1 200 OK\r\nServer: DHMO\r\nDate: {}\r\nContent-Length: 0\r\n\r\n", Time::now()?.as_imf_fixdate()).as_bytes())?;
+			if let Some(end_index) = request_uri.rfind(' ') {
+				if &request_uri[end_index..] == " HTTP/1.1" {
+					if let Some(start_index) = request_uri.find(' ') {
+						if &request_uri[..start_index + 1] == "GET " {
+							stream.write_all(&get_response(HttpStatus::Ok, Some(vec!["Content-Type: text/plain"]), Some(&request_uri[start_index+1..end_index]))?)?;
+						} else {
+							stream.write_all(&get_response(HttpStatus::MethodNotAllowed, None, None)?)?;
+						}
+					} else {
+						stream.write_all(&get_response(HttpStatus::BadRequest, None, None)?)?;
+					}
+				} else {
+					stream.write_all(&get_response(HttpStatus::HTTPVersionNotSupported, None, None)?)?;
+				}
+			} else {
+				stream.write_all(&get_response(HttpStatus::BadRequest, None, None)?)?;
+			}
 		} else {
 			logger.error("Failed to read stream")?;
 		}
