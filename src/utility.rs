c3248f8e0da3da73317ee50187f626db8a2c73d7
H2Owater425 <h2o@dhmo.kr>
2024-09-05 11:25:12 +0900

    feat: Add is_domain

--- src/utility.rs
+++ src/utility.rs
@@ -1,6 +1,13 @@
 #[macro_export]
 macro_rules! send {
 	($stream:expr, $status:expr, $header:expr, $body:expr) => {
-		$stream.write_all(&get_response($status, $header, $body)?)?
+		$stream.write_all(&get_response($status, $header, $body).unwrap()).unwrap();
+		$stream.flush().unwrap();
 	};
 }
+
+pub fn is_domain(domain: &str) -> bool {
+	domain.chars().all(|c| c.is_ascii_lowercase() || c.is_ascii_digit() || c == '.' || c == '-')
+}
+
+// TODO: Add optional macro
